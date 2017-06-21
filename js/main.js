570363b55308069669bf98a2dbb60fe14ee46826
Mone Rivas <mone.rivas@gmail.com>
2017-06-21 14:37:10 -0500

    Estructura HTML index, maquetado de secciones de servicios y proyectos

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/*----Inicio Side Navbar----*/
+
+// Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  $('.collapsible').collapsible();
+
+$('.button-collapse').sideNav({
+      menuWidth: 300, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+/*----Fin Side Navbar----*/
