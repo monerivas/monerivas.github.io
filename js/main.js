479475922981d2810bd40b1172f82cefb4ba05fb
Mone Rivas <mone.rivas@gmail.com>
2017-06-22 13:06:44 -0500

    parallax aboutme

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 /*----Inicio Side Navbar----*/
 
 // Initialize collapse button
-  $(".button-collapse").sideNav();
-  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
-  $('.collapsible').collapsible();
+$(".button-collapse").sideNav();
+// Initialize collapsible (uncomment the line below if you use the dropdown variation)
+$('.collapsible').collapsible();
 
 $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 300
-      edge: 'left', // Choose the horizontal origin
-      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      draggable: true // Choose whether you can drag to open on touch screens
-    }
-  );
+    menuWidth: 300, // Default is 300
+    edge: 'left', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+});
 /*----Fin Side Navbar----*/
 
 
+/*-------Inicio Parallax--------*/
 
-
+$(document).ready(function () {
+    $('.parallax').parallax();
+});
+/*-------Fin Parallax--------*/
